6434792dd098098272c9a2a20ea6f2ad13df336b
Ashley <ajbrown_88@yahoo.com>
2023-09-13 12:23:02 -0400

    Part 1 Complete

--- views/New.jsx
+++ views/New.jsx
@@ -2,6 +2,10 @@ const React = require("react")
 
 class New extends React.Component {
   render() {
+    const date = new Date();
+    const futureDate = date.getDate() + 365;
+    date.setDate(futureDate);
+    const defaultValue = date.toLocaleDateString('en-CA');
     return (
       <div>
         <h1>New Flight Page</h1>
@@ -9,9 +13,9 @@ class New extends React.Component {
 
         <form action="/flight" method="POST">
           Airline: <input type="text"  name="airline" /> <br />
-          FlightNo: <input type="number" name="flightNo" /> <br />
-          Departs: < input type="date" name="departs"  /> <br />
-        <input type="submit" value="Create Flight" />
+          FlightNo: <input type="number" name="flightNo" min={10} max={9999} /> <br />
+          Departs: < input type="datetime-locale" name="departs" default={defaultValue} /> <br />
+        <input type="submit" value="Submit New Flight" />
         </form>
         <nav>
           <a href="/flight">Back</a>
